8edd7461cf18da77b276e1bb2d33dbc0480cbcd0
Hayatevel <yanakin8810@icloud.com>
2023-11-08 23:23:45 +0900

    Forth commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,3 +5,9 @@ from .models import PhotoLibrary
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_image(self, request):
+        images = PhotoLibrary.objects.all()
+        return render(request, self.template_name, {'images': images})
+    
+    
