37d6fd49807d4723d4cd50f5255bdf3ccddd4316
Hayatevel <yanakin8810@icloud.com>
2023-11-22 12:57:22 +0900

    Deploy commit 6

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -124,7 +124,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = '/home/ubuntu/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
 
 MEDIA_URL = '/media/'
 
