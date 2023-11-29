f11002c0946b81a4d2342572825e0fa5e0850dce
Hayatevel <yanakin8810@icloud.com>
2023-11-29 14:41:26 +0900

    Deploy commit 11

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/ubuntu/PythonFramework/myproject/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
